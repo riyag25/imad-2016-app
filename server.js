8676673e627d0c2987d7c9b67b5812a7199e3861
riyag25 <riyageorge25@gmail.com>
2016-11-02 12:11:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/counter',function(req,res){
+    res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -85,9 +89,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var counter=0;
-app.get('/counter',function(req,res){
-    res.send(counter.toString());
-});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {

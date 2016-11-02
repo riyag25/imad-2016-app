24c74152856c3ae03a329af97047567c0aa5fe5a
riyag25 <riyageorge25@gmail.com>
2016-11-02 14:15:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,11 +80,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var counter=0;
-app.get('/counter',function(req,res){
-    counter++;
-    res.send(counter.toString());
-});
+
 
 
 app.get('/:articleName',function(req,res){
@@ -92,7 +88,11 @@ app.get('/:articleName',function(req,res){
   res.send(createTemplate(articles[articleName])); 
 });
 
-
+var counter=0;
+app.get('/counter',function(req,res){
+    counter++;
+    res.send(counter.toString());
+});
 
 
 app.get('/ui/main.js', function (req, res) {
